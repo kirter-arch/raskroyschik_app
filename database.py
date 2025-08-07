53a21c63186f51365521bb7e3a8587506ccbe7bf
Kir личный <x500oog@gmail.com>
2025-08-07 16:56:32 +0400

    баг с БД

--- database.py
+++ database.py
@@ -2,18 +2,15 @@ import streamlit as st
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-# Приложение будет пытаться получить URL только из секретов Streamlit
-# Если secrets.toml нет, то возникнет ошибка, и это правильно, 
-# потому что приложение не должно работать без подключения к Supabase
-DATABASE_URL = st.secrets["DATABASE_URL"]
+try:
+    # Эта строка будет работать, когда вы запустите приложение в Streamlit Cloud
+    DATABASE_URL = st.secrets["DATABASE_URL"]
+except:
+    # Эта строка будет использоваться для локального запуска (например, для create_db.py)
+    DATABASE_URL = "postgresql://postgres:x500oo@localhost:5432/vitrium_db"
 
-# Создаем движок базы данных
 engine = create_engine(DATABASE_URL)
-
-# Создаем базовый класс для наших моделей
 Base = declarative_base()
-
-# Создаем сессию
 Session = sessionmaker(bind=engine)
 
 def get_db():
